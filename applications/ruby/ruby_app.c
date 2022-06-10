a2fa80771c6b5e7ec7883aa593b6fe932f359e02
Justin Collins <justin@presidentbeef.com>
2022-06-09 23:08:39 -0700

    First pass at using file selection dialog

--- applications/ruby/ruby_app.c
+++ applications/ruby/ruby_app.c
@@ -1,30 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dialogs/dialogs.h>
+#include <storage/storage.h>
 #include "mrubyc.h"
 
 #define MEMORY_SIZE (1024*30)
 static uint8_t memory_pool[MEMORY_SIZE];
 
 #define TAG "Ruby"
-#ifdef __cplusplus
-extern const uint8_t ruby_test[];
-#endif
-const uint8_t ruby_test[] = {
-0x52,0x49,0x54,0x45,0x30,0x32,0x30,0x30,0x00,0x00,0x00,0x6a,0x4d,0x41,0x54,0x5a,
-0x30,0x30,0x30,0x30,0x49,0x52,0x45,0x50,0x00,0x00,0x00,0x4e,0x30,0x33,0x30,0x30,
-0x00,0x00,0x00,0x42,0x00,0x01,0x00,0x04,0x00,0x00,0x00,0x00,0x00,0x0c,0x14,0x01,
-0x51,0x02,0x00,0x2f,0x01,0x00,0x01,0x39,0x01,0x6b,0x00,0x01,0x00,0x00,0x19,0x48,
-0x65,0x6c,0x6c,0x6f,0x20,0x66,0x72,0x6f,0x6d,0x20,0x52,0x75,0x62,0x79,0x2c,0x20,
-0x46,0x6c,0x69,0x70,0x70,0x65,0x72,0x21,0x00,0x00,0x01,0x00,0x04,0x70,0x75,0x74,
-0x73,0x00,0x45,0x4e,0x44,0x00,0x00,0x00,0x00,0x08,
-};
 
 int32_t ruby_app(void* p)
 {
   UNUSED(p);
+  FURI_LOG_I(TAG, "Picking Ruby File");
+
+  string_t file_path;
+  string_init(file_path);
+  string_set_str(file_path, "/any");
+
+  DialogsApp* dialogs = furi_record_open("dialogs");
+  bool res = dialog_file_browser_show(
+      dialogs,
+      file_path,
+      file_path,
+      "mrb",
+      true,
+      &I_music_10px,
+      false);
+
+  furi_record_close("dialogs");
+  if(!res) {
+    FURI_LOG_E(TAG, "No file selected");
+    string_clear(file_path);
+    return 0;
+  }
+
+  FURI_LOG_I(TAG, "Loading Ruby bytecode");
+  Storage* storage = furi_record_open("storage");
+  File* file = storage_file_alloc(storage);
+
+  if(!storage_file_open(file, string_get_cstr(file_path), FSAM_READ, FSOM_OPEN_EXISTING)) {
+    FURI_LOG_E(TAG, "Unable to open file");
+    storage_file_free(file);
+    furi_record_close("storage");
+    return 0;
+  };
+
+  uint64_t file_size = storage_file_size(file);
+  uint8_t *ruby_test = malloc(file_size);
+
+  storage_file_read(file, ruby_test, file_size);
+
   FURI_LOG_I(TAG, "Starting Ruby!!!");
   mrbc_init(memory_pool, MEMORY_SIZE);
-  
+
   if( mrbc_create_task(ruby_test, 0) != NULL ){
     FURI_LOG_I(TAG, "Running Ruby!!!");
     mrbc_run();
@@ -34,5 +63,11 @@ int32_t ruby_app(void* p)
   }
 
   FURI_LOG_I(TAG, "Ending Ruby!!!");
+  storage_file_free(file);
+  string_clear(file_path);
+  furi_record_close("storage");
+  free(ruby_test);
+  FURI_LOG_I(TAG, "Done.");
+
   return 0;
 }
